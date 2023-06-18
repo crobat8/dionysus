7876369213bf5a1f3371f4278579121facf3374d
crobat8 <133931076+crobat8@users.noreply.github.com>
2023-06-17 19:45:29 -0700

    finished building table to go with map

--- src/componets/Parties.jsx
+++ src/componets/Parties.jsx
@@ -3,6 +3,7 @@ import {  db  } from "../firebase";
 import { collection, query, where, getDocs} from "firebase/firestore";
 import { AuthContext } from "../context/AuthContext";
 
+import my from "../img/person.png"
 
 import { GoogleMap, useJsApiLoader, MarkerF } from "@react-google-maps/api";
 
@@ -73,52 +74,72 @@ const Parties = () =>{
                     
                 </ul>
             </div>
+            
             <div className='right'>
-                {parties.map((e)=>{
-                    console.log(e)
-                    var center = {lat: e.data.Lattitude, lng: e.data.Longitude}
-                    //const personIcon = "https://developers.google.com/maps/documentation/javascript/examples/full/images/info-i_maps.png";
+                <div className="map" >
+                    
+                
+                <GoogleMap
+                    
+                    center={me}
+                    zoom={5}
+                    mapContainerStyle={{ width: '100%', height: '100%' }}
+                    options={{
+                        zoomControl: false,
+                        streetViewControl: false,
+                        mapTypeControl: false,
+                        fullscreenControl: false,
+                    }}
+                    onLoad={map => setMap(map)}
+                >
+                
+                {parties.map((e,i)=>{
+                    
+                    var Loc = {lat: e.data.Lattitude, lng: e.data.Longitude}
                     return(
-                        <article className="party" >
-                            <h2>
-                                {e.data.Title}
-                            </h2>
-                            <h3>
-                                {e.data.EventType}
-                            </h3>
-                            <div style={{ "height" : "70%" , "width" : "100%"}}>
-                                <GoogleMap
-                                    center={center}
-                                    zoom={15}
-                                    mapContainerStyle={{ width: '100%', height: '100%' }}
-                                    options={{
-                                        zoomControl: false,
-                                        streetViewControl: false,
-                                        mapTypeControl: false,
-                                        fullscreenControl: false,
-                                    }}
-                                    onLoad={map => setMap(map)}
-                                >
-                                    <MarkerF 
-                                    position={center} 
-                                    />
-                                    <MarkerF 
-                                    position={me} 
-                                    />
+                        <MarkerF 
 
-                                </GoogleMap> 
-                            </div>
-                            <p>
-                                currently have: {e.data.Attending}
-                            </p>
-                            <p>
-                                looking to have have: {e.data.Wanted}
-                            </p>
-                        </article>
-                        
-                        
+                            position={Loc} 
+                            key={i}
+                        />         
                     )
                 })}
+                
+                <MarkerF 
+                    icon={my}
+                    position={me} 
+                />
+
+                </GoogleMap>
+                </div>
+                <table class="table" >
+                        <thead>
+                            <tr className="header">
+                                <th>Event Type</th>
+                                <th>Title</th>
+                                <th>people coming</th>
+                                <th>people wanted</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {parties.map((e,i)=>{
+                                console.log(e)
+                
+                                console.log(i)
+                                return(
+                                <tr key={i} className="line">
+                                    <td>{e.data.EventType}</td>
+                                    <td>{e.data.Title}</td>
+                                    <td>{e.data.Attending}</td>
+                                    <td>{e.data.Wanted}</td>
+                                </tr>        
+                            )
+                            })}
+                            
+                        </tbody>
+                        
+                    </table>
+                   
             </div>
         </div>
     )

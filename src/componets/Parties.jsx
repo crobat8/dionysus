b63dfdeedebe2df57a2381ec406fabde0d6c316c
crobat8 <133931076+crobat8@users.noreply.github.com>
2023-06-27 01:06:42 -0700

    filter is reading user input

--- src/componets/Parties.jsx
+++ src/componets/Parties.jsx
@@ -41,6 +41,7 @@ if ("geolocation" in navigator) {
     me.lat=position.coords.latitude;
     me.lng=position.coords.longitude
   });
+  console.log(me)
 } else {
   console.log("Not Available");
 }
@@ -59,8 +60,34 @@ const Parties = () =>{
   const [choseSlide,setChoseSlide] =useState(0);
   const [myData,setMyData]     = useState([]);
   const [friends,setFriends]   = useState([]);
-    
-  function handleSlide(x){
+  const [milesSlider,setmilesSlider] =useState(10);
+  const [genreFilter,setGenreFilter] =useState(["test"]);
+  
+  function arrayRemove(genre,value){
+    return genre != value
+  }
+
+  function handleGenre(input){
+    console.log(input)
+    var value = input.target.value
+    console.log(value)
+    if(input.target.checked){
+      setGenreFilter([...genreFilter,{value}])
+      
+    }else{
+      console.log(value)
+      setGenreFilter(genreFilter.filter(genre => genre.value != value))
+      
+    }
+    //
+    console.log(genreFilter)
+  }
+
+  function handleRange(input){
+    setmilesSlider(input )
+  }
+
+  function handleDropDown(x){
     setChoseSlide(x.i);
   }
   
@@ -77,7 +104,10 @@ const Parties = () =>{
   }
 
   function getParties(){
-    const colRef =collection(db,"Event")
+    console.log(genreFilter)
+    const colRef =query(collection(db,"Event")
+                  ,where("EventType","in",genreFilter.value)
+                  )
     onSnapshot(colRef,(snapshot)=>{
       setParties(snapshot.docs.map(doc=>doc.data()))
       setLoading(false);
@@ -167,6 +197,9 @@ const Parties = () =>{
     }
   }
 
+
+  
+
   if(loading||!isLoaded||loading2||loading3){
     getParties();
     getMe();
@@ -183,6 +216,128 @@ const Parties = () =>{
         <h2>
           filter
         </h2>
+        <div className="range">
+          <input 
+            id="distance"
+            className="distance" 
+            type="range" 
+            min="1" 
+            max="50" 
+            onChange={(event)=>handleRange(event.target.value)}
+            step="1"
+            defaultValue={10}
+          />
+          <p>
+            search range: {milesSlider} miles
+          </p> 
+        </div>
+        
+        <div className="events">
+        
+          <h3>
+            Event Types:
+          </h3>
+          <form >
+            <div>
+              <input
+                className="form-check-input"
+                type="checkbox"
+                name="languages"
+                value="Sports"
+                id="flexCheckDefault"
+                onChange={(event) => handleGenre(event)}
+              />
+              <label
+                className="form-check-label"
+              >
+                Sports
+              </label>
+            </div>
+            <div>
+              <input
+                className="form-check-input"
+                type="checkbox"
+                name="languages"
+                value="House Party"
+                id="flexCheckDefault"
+                onChange={(event) => handleGenre(event)}
+              />
+              <label
+                className="form-check-label"
+              >
+                House Party
+              </label>
+            </div>
+            <div>
+              <input
+                className="form-check-input"
+                type="checkbox"
+                name="languages"
+                value="Yard Games"
+                id="flexCheckDefault"
+                onChange={(event) => handleGenre(event)}
+              />
+              <label
+                className="form-check-label"
+              >
+                Yard Games
+              </label>
+            </div>
+            <div>
+              <input
+                className="form-check-input"
+                type="checkbox"
+                name="languages"
+                value="Hit The Town"
+                id="flexCheckDefault"
+                onChange={(event) => handleGenre(event)}
+              />
+              <label
+                className="form-check-label"
+              >
+                Hit The Town
+              </label>
+            </div>
+            <div>
+              <input
+                className="form-check-input"
+                type="checkbox"
+                name="languages"
+                value="Video Games"
+                id="flexCheckDefault"
+                onChange={(event) => handleGenre(event)}
+              />
+              <label
+                className="form-check-label"
+              >
+                Video Games
+              </label>
+            </div>
+            <div>
+              <input
+                className="form-check-input"
+                type="checkbox"
+                name="languages"
+                value="Board Games"
+                id="flexCheckDefault"
+                onChange={(event) => handleGenre(event)}
+              />
+              <label
+                className="form-check-label"
+              >
+                Board Games
+              </label>
+            </div>
+          </form>
+          {genreFilter.map((e)=>{
+            
+            return(
+              <p>
+                {e.value}
+              </p>
+            )
+          })}
+        </div>
       </div>
       <div className='right'>
         <div className="map" >
@@ -211,13 +366,13 @@ const Parties = () =>{
                   position={Loc} 
                   key={i}
                   zIndex={75}
-                  onClick={() => handleSlide({i})}
+                  onClick={() => handleDropDown({i})}
                   label={i.toString()}
                   >
                     <InfoWindowF
                       position={Loc} 
                       key={i}
-                      onCloseClick={(close) => handleSlide({close})}
+                      onCloseClick={(close) => handleDropDown({close})}
                     >
                       <span>
                         {e.Address}
@@ -234,7 +389,7 @@ const Parties = () =>{
                   position={Loc} 
                   key={i}
                   zIndex={i}
-                  onClick={() => handleSlide({i})}
+                  onClick={() => handleDropDown({i})}
                   label={i.toString()}
                   >
                   </MarkerF>            
@@ -269,7 +424,7 @@ const Parties = () =>{
               if(i === choseSlide){
                 return(
                   <div  className="FullParty" style={{backgroundColor:"#00618c"}}>
-                    <tr key={i} className="line" onClick={() => handleSlide({i})}>
+                    <tr key={i} className="line" onClick={() => handleDropDown({i})}>
                       <td>{i}</td>
                       <td>{e.EventType}</td>
                       <td>{e.Title}</td>
@@ -283,7 +438,7 @@ const Parties = () =>{
               }else{
                 return(
                   <div  className="FullParty" >
-                    <tr key={i} className="line" onClick={() => handleSlide({i})}>
+                    <tr key={i} className="line" onClick={() => handleDropDown({i})}>
                       <td>{i}</td>
                       <td>{e.EventType}</td>
                       <td>{e.Title}</td>
